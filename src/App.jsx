86d4638435e4ba854dafb3dc8278d885fe709751
Bostjan <bostjanzup@gmail.com>
2021-12-15 20:51:04 +0100

    sign in and sign up forms

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Home from "./components/Home";
 import SectorList from "./components/sector/SectorList";
 import SectorDetails from "./components/sector/SectorDetails";
 import RouteDetails from "./components/route/RouteDetails";
+import SignUp from "./components/auth/SignUp";
+import SignIn from "./components/auth/SignIn";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
           <h1 className="text-center mt-4">β Route</h1>
           <Routes>
             <Route index element={<Home />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
             <Route path="/home" element={<Home />} />
             <Route path="/crags/all" element={<CragList />} />
             <Route path="/crags/:cragId" element={<SectorList />} />
--- src/components/layout/SignedOutLinks.jsx
+++ src/components/layout/SignedOutLinks.jsx
@@ -4,10 +4,10 @@ const SignedOutLinks = () => {
     return (
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav ms-auto">
-                <Link className="nav-link" to="/">
+                <Link className="nav-link" to="/signup">
                     Sign Up
                 </Link>
-                <Link className="nav-link" to="/">
+                <Link className="nav-link" to="/signin">
                     Login
                 </Link>
 
